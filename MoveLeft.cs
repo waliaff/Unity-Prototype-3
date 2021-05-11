76018cd998b23acc51453bb41c046cde01714a41
waliaff <72561297+waliaff@users.noreply.github.com>
2021-05-11 22:52:57 +0530

    Add files via upload

---
+++ MoveLeft.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private PlayerController playerControllerScript;
+    private float speed = 10;
+    private float leftBound = -15.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent <PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ RepeatBackground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 repeatBackground;
+    private float repeatWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        repeatBackground = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < repeatBackground.x -repeatWidth)
+        {
+            transform.position = repeatBackground;
+        }
+    }
+}
